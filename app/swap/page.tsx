990b3435c912fe468d64e8ab2f995b998ca37b91
Herodes Lugo <herodeslugo@gmail.com>
2024-02-08 15:57:40 -0400

    responsive design

---
+++ app/swap/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Probando Modal
+
+        <button className='text-white p-6 bg-gray-700'>Presioname</button>
+
+    </div>
+  )
+}
+
+export default page
