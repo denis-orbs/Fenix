407794e36f2b4707bde25ccc685e3ae435f8b40a
josesantana.web <josesantana.web@gmail.com>
2024-02-09 16:08:14 -0400

    fix: search bar

--- components/Common/Search/index.tsx
+++ components/Common/Search/index.tsx
@@ -2,12 +2,12 @@
 
 const Search = () => {
   return (
-    <div className="relative flex items-center w-full xl:w-1/3 h-[62px] p-2 rounded-lg box-large">
+    <div className="relative flex items-center w-full h-[62px] p-2 rounded-lg bg-shark-400 bg-opacity-40">
       <span className="flex items-center justify-center w-8 h-5 text-2xl icon-search text-shark-100" />
       <input
         type="text"
         placeholder="Search by name, symbol or address..."
-        className="px-2 text-sm bg-transparent outline-none text-shark-100"
+        className="px-2 text-sm bg-transparent outline-none text-shark-100 w-full"
       />
     </div>
   )
