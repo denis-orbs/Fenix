d84f24d12820ff50d70a898c1d8219d3b9284bab
taypequantumlabx <taype@quantumlabx.io>
2024-02-14 20:45:20 -0400

    Refactor landing page layout and image styling

--- components/Landing/HowItWorks/index.tsx
+++ components/Landing/HowItWorks/index.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image'
 
 const HowItWorks = () => {
   return (
-    <div className="relative w-full mb-10">
-      <div className="absolute left-[-360px] bottom-20">
+    <div className="relative w-full mb-10 px-5">
+      <div className="absolute left-[-360px] bottom-20 z-10">
         <Image
           src="/static/images/landing/howitworks/meteor.png"
           alt="img"
           width={733}
           height={560}
-          className="w-full"
+          className="w-full object-contain"
         />
       </div>
       <p className="text-white text-lg mb-2 text-center font-light">How it Works</p>
@@ -19,13 +19,13 @@ const HowItWorks = () => {
       <p className="text-white text-base text-center mb-10">
         Model designed to reward participants that enable sustainable growth for protocol
       </p>
-      <div className="mx-auto flex justify-center">
+      <div className="mx-auto flex justify-center relative z-50">
         <Image
           src="/static/images/landing/howitworks/steps.png"
           alt="img"
           width={733}
           height={560}
-          className="w-[733px] h-[560px] rounded-lg"
+          className="w-[733px] h-[560px] rounded-lg object-contain"
         />
       </div>
     </div>
--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -11,7 +11,7 @@ const Landing = () => {
   return (
     <main>
       <Info />
-      <div className="w-full relative">
+      <div className="w-full relative max-md:pt-[160px]">
         {/* <div className="w-[163px] h-[163px] bg-black rounded-full border-[2px] border-white absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2" /> */}
 
         {/* <Image
@@ -25,8 +25,8 @@ const Landing = () => {
           <Lottie animationData={animation} />
         </div>
 
-        <div className="absolute left-1/2 -translate-x-1/2 top-[-100px] md:top-[-7px] z-50">
-          <Image src="/static/images/landing/support/partners.svg" alt="img" width={366} height={143} className="" />
+        <div className="absolute left-1/2 -translate-x-1/2 top-0 md:top-[-7px] z-[999] backdrop-blur-[38px]">
+          <Image src="/static/images/landing/support/partners.svg" alt="img" width={366} height={143} className="max-md:h-[111px] max-md:w-[300px]" />
         </div>
 
         {/* <div className="mx-auto flex justify-center absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-[2244px] mix-blend-color-dodge">
