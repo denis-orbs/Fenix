c2e91a3870e68244e0edea51fe7f4ea7aaaa3d07
josesantana.web <josesantana.web@gmail.com>
2024-02-19 17:13:52 -0400

    fix: prop option

--- components/Common/Filter/index.tsx
+++ components/Common/Filter/index.tsx
@@ -3,23 +3,21 @@
 import { Button } from '@/components/UI'
 
 interface FilterProps {
-  Options: string[]
+  options: string[]
   currentTab: string
   setCurrentTab: (parameter: string) => void
 }
 
-const Filter = ({ Options, currentTab, setCurrentTab }: FilterProps) => {
-  // console.log(currentTab)
+const Filter = ({ options, currentTab, setCurrentTab }: FilterProps) => {
 
   const handlerChange = (parameter: string) => {
     setCurrentTab(parameter)
   }
   return (
     <div
-      className="flex flex-col xl:flex-row items-center gap-2 px-3 py-2 filter-box 
-    rounded-lg justify-start md:gap-5 w-full xl:w-2/3  bg-opacity-40"
+      className="flex flex-col items-center justify-start w-full gap-2 px-3 py-2 rounded-lg xl:flex-row filter-box md:gap-5 xl:w-2/3 bg-opacity-40"
     >
-      {Options.map((option, index) => {
+      {options.map((option, index) => {
         return (
           <Button
             key={index}
