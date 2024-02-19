cfb92b4ce314711cc2f060faf73b80bbcad5e435
josesantana.web <josesantana.web@gmail.com>
2024-02-19 19:16:47 -0400

    feat: add progress ba

--- components/UI/index.tsx
+++ components/UI/index.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 import Button from './Button'
 import Modal from './Modal'
-import Tooltip from './Tooltip/'
-import Select from './Select/'
-import Pagination from './Pagination/'
+import Tooltip from './Tooltip'
+import Select from './Select'
+import Pagination from './Pagination'
 import PaginationMobile from './Pagination/PaginationMobile'
-import Switch from './Switch/'
+import Switch from './Switch'
+import ProgressBar from './ProgressBar'
 import { TableBody, TableHead, TableCell, TableRow, TableSkeleton } from './Table'
 
 export {
@@ -21,4 +22,5 @@ export {
   Select,
   Switch,
   PaginationMobile,
+  ProgressBar,
 }
