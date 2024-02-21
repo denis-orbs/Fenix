4e10a39d3832a5994fcde405e63f70fd67f2092d
taypequantumlabx <taype@quantumlabx.io>
2024-02-21 17:46:25 -0400

    a

---
+++ app/deposit-liquidity/page.tsx
@@ -0,0 +1,15 @@
+import DepositLiquidity from '@/components/DepositLiquidity'
+import Decorator from '@/components/Common/Layout/Background'
+
+const DepositLiquidityPage = () => {
+  return (
+    <main>
+      <div className="container">
+        <DepositLiquidity />
+        <Decorator />
+      </div>
+    </main>
+  )
+}
+
+export default DepositLiquidityPage
