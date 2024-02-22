8fafa9cf2b94d972d891254423c93b5df43bd653
taypequantumlabx <taype@quantumlabx.io>
2024-02-22 12:13:23 -0400

    Add Chart component to Deposit and Positions pages

--- components/Liquidity/Positions/index.tsx
+++ components/Liquidity/Positions/index.tsx
@@ -1,16 +1,15 @@
 'use client'
 
 import Panel from '@/components/Liquidity/Deposit/Panel'
+import Chart from '@/components/Chart'
 
 const Positions = () => {
   return (
     <div className="flex flex-col items-start gap-6 mb-4 xl:gap-10 xl:flex-row">
       <div className="flex flex-col w-full">
-        <div className="flex flex-wrap items-start w-full gap-5 mb-10 xl:flex-nowrap">
+        <div className="flex flex-wrap w-full gap-5 mb-10 xl:flex-nowrap">
           <Panel />
-          <div className="w-full xl:w-full bg-shark-400 opacity-40 h-[585px] rounded-2xl text-white flex items-center justify-center">
-            GRAFICA Positions
-          </div>
+          <Chart className="w-full xl:w-full" />
         </div>
       </div>
     </div>
