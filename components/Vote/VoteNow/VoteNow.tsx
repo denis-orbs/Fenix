552df3892b12d3c4fa0f85148886ae0cda455cce
josesantana.web <josesantana.web@gmail.com>
2024-02-26 11:28:39 -0400

    fix: delete mini box

--- components/Vote/VoteNow/VoteNow.tsx
+++ components/Vote/VoteNow/VoteNow.tsx
@@ -20,29 +20,24 @@ const VoteNow = ({ openModal, setOpenModal, activeVote }: VoteNowProps) => {
       <div className="flex flex-col justify-center w-full rounded-2xl xl:rounded-none relative z-10">
         <div className="flex items-center mb-3 justify-evenly">
           <h4 className="w-full mb-3 text-sm text-white">Vote now</h4>
-          {/* <span className="text-2xl icon-reflesh text-shark-100"></span> */}
         </div>
         <div className="relative flex flex-col w-auto">
-          {activeVote ? (
-            <ActiveVote handlerChange={handlerChange}/>
-          ) : (
-           <InactiveVote handlerChange={handlerChange}/>
-          )}
+          {activeVote ? <ActiveVote handlerChange={handlerChange} /> : <InactiveVote handlerChange={handlerChange} />}
         </div>
-        <div className="flex gap-3 p-1 mt-5">
-          <div className="mini-box-vote text-sm flex flex-col items-center justify-center w-[30%]">
+        <div className="flex gap-3 p-1 mt-2">
+          <div className="relative bg-shark-400 bg-opacity-40 rounded-xl pl-4 xl:pl-5 pr-4 xl:pr-3 py-3 text-sm flex flex-col items-center justify-center w-[30%]">
             <p className="text-shark-100">Fenix Balance</p>
             <p className="text-white text-2xl">0.00</p>
           </div>
-          <div className="mini-box-vote text-sm flex flex-col  items-center w-[30%]">
+          <div className="relative bg-shark-400 bg-opacity-40 rounded-xl pl-4 xl:pl-5 pr-4 xl:pr-3 py-3 text-sm flex flex-col  items-center w-[30%]">
             <p className="text-shark-100">Emissions / % of Vote</p>
             <p className="text-2xl text-white">0%</p>
           </div>
           <div className="w-[40%] flex flex-col gap-3">
-            <div className="flex text-xs text-white mini-box-vote justify-evenly">
+            <div className="flex text-xs text-white relative bg-shark-400 bg-opacity-40 w-full rounded-lg pl-4 xl:pl-5 pr-4 xl:pr-3 py-3 justify-evenly">
               <p className="text-shark-100">Vating Apr</p> <p>0%</p>
             </div>
-            <div className="flex text-xs text-white mini-box-vote justify-evenly">
+            <div className="flex text-xs text-white relative bg-shark-400 bg-opacity-40 w-full rounded-lg pl-4 xl:pl-5 pr-4 xl:pr-3 py-3 justify-evenly">
               <p className="text-shark-100">Epoch 1</p> <p className="text-white">6d 12h 43m</p>
             </div>
           </div>
