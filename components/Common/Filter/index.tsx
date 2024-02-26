50adece0c6f005c3426721d58120c550a45f3ed3
josesantana.web <josesantana.web@gmail.com>
2024-02-26 10:07:47 -0400

    fix: responsive liquidity

--- components/Common/Filter/index.tsx
+++ components/Common/Filter/index.tsx
@@ -1,62 +1,45 @@
 'use client'
 
 import { Button } from '@/components/UI'
+import cn from '@/utils/cn'
 
 interface FilterProps {
   options: string[]
   currentTab: string
-  bgBox?: string 
-  justifyCenter?: string
+  bgBox?: string
+  align?: 'center' | 'start' | 'end'
   setCurrentTab: (parameter: string) => void
 }
 
-const Filter = ({ options, currentTab, setCurrentTab, bgBox = "", justifyCenter = "" }: FilterProps) => {
-
+const Filter = ({ options, currentTab, setCurrentTab, bgBox = '', align = "start" }: FilterProps) => {
   const handlerChange = (parameter: string) => {
     setCurrentTab(parameter)
   }
+
+  const mergeClassName = cn(
+    'flex flex-wrap items-center w-full gap-2 px-3 py-2 rounded-lg xl:flex-row 2xl:gap-3 xl:w-full bg-opacity-40',
+    align === 'center' ? 'justify-center' : align === 'start' ? 'justify-start' : 'justify-end',
+    bgBox === '' ? 'filter-box' : bgBox
+  )
+
   return (
     <>
-    <div
-      className={`lg:flex hidden  flex-wrap items-center justify-center xl:${justifyCenter === "" ? "justify-start": justifyCenter } w-full gap-2 px-3 
-      py-2 rounded-lg xl:flex-row ${bgBox === "" ? "filter-box" : bgBox} md:gap-5 xl:w-full bg-opacity-40`}
-    >
-      {options.map((option, index) => {
-        return (
-          <Button
-            key={index}
-            onClick={()=>{
-              handlerChange(option.toUpperCase())
-            }}
-            variant={currentTab === option.toString().toUpperCase() ? 'primary' : 'default'}
-            className="h-[40px]  md:h-auto w-2/5  xl:w-auto"
-          >
-            {option}
-          </Button>
-        )
-      })}
-    </div>
-    {/* Desktop */}
-    <div
-      className={`flex lg:hidden  flex-wrap items-center justify-center xl:${justifyCenter === "" ? "justify-start": justifyCenter } w-full gap-2 px-3 
-      py-2 rounded-lg xl:flex-row ${bgBox === "" ? "filter-box" : bgBox} md:gap-5 xl:w-full bg-opacity-40`}
-    >
-      {options.map((option, index) => {
-        return (
-          <Button
-            key={index}
-            onClick={()=>{
-              handlerChange(option.toUpperCase())
-            }}
-            variant={currentTab === option.toString().toUpperCase() ? 'primary' : 'tertiary'}
-            className="h-[40px]  md:h-auto w-2/5  xl:w-auto"
-          >
-            {option}
-          </Button>
-        )
-      })}
-    </div>
-    {/* Mobile */}
+      <div className={mergeClassName}>
+        {options.map((option, index) => {
+          return (
+            <Button
+              key={index}
+              onClick={() => {
+                handlerChange(option.toUpperCase())
+              }}
+              variant={currentTab === option.toString().toUpperCase() ? 'primary' : 'tertiary'}
+              className="h-[40px] md:h-auto w-full md:w-auto"
+            >
+              {option}
+            </Button>
+          )
+        })}
+      </div>
     </>
   )
 }
