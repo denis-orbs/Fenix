b8e6197a697f8c9a9926956040022dab7ebfb10f
josesantana.web <josesantana.web@gmail.com>
2024-02-26 11:42:46 -0400

    fix: menu options active

--- components/Lock/Common/Deposit/index.tsx
+++ components/Lock/Common/Deposit/index.tsx
@@ -52,7 +52,7 @@ const DepositLock = () => {
             <div className="flex items-center gap-2">
               <div className="flex relative items-center justify-center w-12 h-12 p-3 rounded-lg bg-shark-400 bg-opacity-60">
                 <span className="icon-lucide inline-block text-2xl text-transparent bg-gradient-to-r from-outrageous-orange-500 to-festival-500 bg-clip-text "></span>
-                <span className='bg-green-600 p-1 rounded-full absolute bottom-2 right-2'></span>
+                <span className="bg-green-600 p-1 rounded-full absolute bottom-2 right-2"></span>
               </div>
               {/* //// */}
               <div>
@@ -93,7 +93,7 @@ const DepositLock = () => {
         </div>
         {/* line black */}
         <div className="relative flex flex-col w-full xl:w-[45%] max-h-[390px]  overflow-x-none border-t-2 xl:border-none border-shark-400">
-          <div className='mt-4'>
+          <div className="mt-4">
             <h1 className="text-white text-xl mb-5">Nest Deposit</h1>
           </div>
           <div>
@@ -117,7 +117,7 @@ const DepositLock = () => {
             </>
           )}
           <div className="mt-16 ">
-            <p className="flex gap-2 justify-center text-shark-100">
+            <p className="flex gap-2 justify-center text-shark-100 cursor-pointer">
               <span className="icon-discord"></span>Need some help?
             </p>
           </div>
