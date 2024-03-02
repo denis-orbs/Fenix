69d9714f19b06c16f80ff6dd8bb33de64a6569ab
josesantana.web <josesantana.web@gmail.com>
2024-03-01 21:27:12 -0400

    fix: fon size modals

--- components/Modals/DeleteStrategy/index.tsx
+++ components/Modals/DeleteStrategy/index.tsx
@@ -14,7 +14,7 @@ const DeleteStrategy = ({ openModal, setOpenModal }: DeleteStrategyProps) => {
       <div className="common-modal">
         <span className="absolute top-0 right-0 text-2xl cursor-pointer icon-x text-shark-100 p-2 2xl:p-0" onClick={handlerClose} />
         <div className="relative z-10 w-auto h-auto">
-          <h1 className="mb-10 text-2xl text-center text-white line-clamp-2 md:line-clamp-3">Are you sure you would like to delete your strategy?</h1>
+          <h1 className="mb-6 text-xl text-center text-white line-clamp-2 md:line-clamp-3">Are you sure you would like to delete your strategy?</h1>
           <p className='text-white text-sm text-center px-6 mb-5 line-clamp-2 md:line-clamp-none'>Deleting your strategy will result in all strategy data being lost and impossible to restore. All funds will be withdrawn to your wallet.</p>
             <div className='flex items-center justify-between bg-shark-400  bg-opacity-40 p-2 py-4 rounded-xl '>
                 <div className='px-3'>
