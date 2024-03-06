c6591505ae32d27c21075a1d6c7f49d5ae37c75a
josesantana.web <josesantana.web@gmail.com>
2024-03-06 09:35:04 -0400

    fix: main video

--- components/Landing/Main/index.tsx
+++ components/Landing/Main/index.tsx
@@ -72,13 +72,9 @@ const Main = () => {
                 className="relative xl:absolute -z-10 top-0 left-1/2 max-xl:-translate-x-1/2 xl:top-[-100px] 2xl:top-[-32px] xl:left-[-123px] min-w-[1480px] object-contain max-md:min-w-[556.986px] max-2xl:min-w-[1000px] max-xl:object-contain"
               />
               <div className="absolute max-xl:left-0 max-xl:top-0 max-xl:right-0 max-xl:bottom-0  xl:w-[1000px] xl:h-[701.41px]  xl:top-[-100px] xl:left-[-123px] 2xl:w-[1480px] 2xl:h-[1037.8px] 2xl:top-[-32px] pointer-events-none select-none">
-                <Image
-                  src="/static/images/landing/main/fenix.png"
-                  width={1200}
-                  height={1200}
-                  className="absolute z-10 mix-blend-lighten left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2"
-                  alt="Fenix"
-                />
+                <video width={733} height={560} loop playsInline autoPlay muted className="absolute z-10 mix-blend-lighten left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-full">
+                  <source src="/static/videos/fenix.mp4" type="video/mp4"></source>
+                </video>
               </div>
             </div>
           </div>
