478e41af2c6b3e9267e6276f88bb18a71cc2157d
0xSather <0xsather@proton.me>
2024-03-18 19:42:24 +0100

    Add fields to IToken interface

--- src/library/types/index.tsx
+++ src/library/types/index.tsx
@@ -1,8 +1,10 @@
 export interface IToken {
+  id?: number // make mandatory
   name: string
   symbol: string
+  address?: string // make mandatory
+  decimals?: number // make mandatory
 }
-
 export interface IOption {
   label: string
   value: string
