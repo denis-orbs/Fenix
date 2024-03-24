68d0738a2539eb0883043238142363e87351ebd0
0xSather <0xsather@proton.me>
2024-03-24 23:26:14 +0100

    Add bridge page

---
+++ src/app/trade/bridge/page.tsx
@@ -0,0 +1,9 @@
+import TradeLayout from '@/src/components/Trade/Common/Layout'
+
+export default function page() {
+  return (
+    <TradeLayout>
+      <iframe height={700} width={'100%'} src="https://fenix-crosschain.vercel.app/"></iframe>
+    </TradeLayout>
+  )
+}
