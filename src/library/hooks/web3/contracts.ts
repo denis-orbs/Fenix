9c4fc8470a188c232d7b163ef87434f2be27d0bb
0xSather <0xsather@proton.me>
2024-03-26 23:29:26 +0100

    Add useContract hook for interacting with smart contracts

---
+++ src/library/hooks/web3/contracts.ts
@@ -0,0 +1,37 @@
+import { useMemo } from 'react'
+import { Abi, Address, WalletClient, getContract } from 'viem'
+import { usePublicClient, useWalletClient } from 'wagmi'
+import useActiveConnectionDetails from './useActiveConnectionDetails'
+import { AddressZero } from '../../constants/misc'
+
+export function useContract<T extends Abi>(
+  addressOrAddressMap: string | { [chainId: number]: string } | null | undefined,
+  ABI?: T
+) {
+  const { chainId } = useActiveConnectionDetails()
+  const publicClient = usePublicClient()
+  const { data: walletClient } = useWalletClient()
+
+  return useMemo(() => {
+    if (!addressOrAddressMap || !ABI || !chainId) return null
+    let address: string | undefined
+    if (typeof addressOrAddressMap === 'string') address = addressOrAddressMap
+    else address = addressOrAddressMap[chainId]
+    if (!address || address === AddressZero) return null
+    try {
+      return getContract({
+        address: address as Address,
+        abi: ABI,
+        client: {
+          public: publicClient,
+          wallet: (walletClient as WalletClient) ?? publicClient,
+        },
+        // client: publicClient,
+        // walletClient: (walletClient as WalletClient) ?? publicClient,
+      })
+    } catch (error) {
+      console.error('Failed to get contract', error)
+      return null
+    }
+  }, [addressOrAddressMap, ABI, chainId, walletClient, publicClient])
+}
