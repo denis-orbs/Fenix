ca257d6c95277b7b2190ef138cfa909913679a6b
0xSather <0xsather@proton.me>
2024-03-28 16:36:45 +0100

    Ichi CLM integration

--- src/components/Liquidity/Deposit/Panel/Concentrated/Automatic/DepositAmountsICHI.tsx
+++ src/components/Liquidity/Deposit/Panel/Concentrated/Automatic/DepositAmountsICHI.tsx
@@ -1,11 +1,36 @@
 import { Button } from '@/src/components/UI'
+import { ichiVaultABI } from '@/src/library/constants/abi'
+import { useERC20Balance } from '@/src/library/hooks/web3/erc20/useERC20Balance'
+import useActiveConnectionDetails from '@/src/library/hooks/web3/useActiveConnectionDetails'
 import Image from 'next/image'
+import { useState } from 'react'
+import { useWriteContract } from 'wagmi'
 
-const DepositAmountsICHI = ({
-  token,
-}: {
-  token: { name: string; symbol: string }
-}) => {
+const DepositAmountsICHI = ({ token }: { token: { name: string; symbol: string } }) => {
+  const token0 = '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619' // Wrapped Ether
+  const [token0DepositAmount, setToken0DepositAmount] = useState('')
+  const { account: userAddress, chain, chainId } = useActiveConnectionDetails()
+  const depositVault = '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619'
+  const { writeContract } = useWriteContract()
+
+  const { tokenBalance: tokenBalance0 } = useERC20Balance({
+    tokenAddress: token0,
+    tokenDecimals: 18,
+    owner: userAddress,
+  })
+  console.log(tokenBalance0?.toString())
+  const createPosition = async () => {
+    try {
+      writeContract({
+        abi: ichiVaultABI,
+        address: depositVault,
+        functionName: 'deposit',
+        args: [],
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className="bg-shark-400 bg-opacity-40 px-[15px] py-[29px] md:px-[19px] border border-shark-950 rounded-[10px] mb-2.5">
       <div className="text-xs leading-normal text-white mb-2">Deposit amounts</div>
--- src/library/constants/abi/index.ts
+++ src/library/constants/abi/index.ts
@@ -1,3 +1,4 @@
 import ERC20_ABI from './ERC20'
 import { gammaUniProxyABI } from './gammaUniProxyABI'
-export { ERC20_ABI, gammaUniProxyABI }
+import { ichiVaultABI } from './ichiVaultABI'
+export { ERC20_ABI, gammaUniProxyABI, ichiVaultABI }
