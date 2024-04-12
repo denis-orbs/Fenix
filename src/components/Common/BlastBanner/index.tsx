41575ef81241840521f7bd0dbcf7a409ead0711b
josesantana.web <josesantana.web@gmail.com>
2024-04-12 16:27:09 -0400

    feat: add banner blast

---
+++ src/components/Common/BlastBanner/index.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { useState } from 'react'
+
+import { Button } from '@/src/components/UI'
+
+const BlastBanner = () => {
+  const [close, setClose] = useState<boolean>(false)
+
+  if (close) return null
+
+  const handlerClose = () => setClose(true)
+
+  return (
+    <div className="blast-banner">
+        <div className="text-white absolute text-sm right-3 top-2 z-20 cursor-pointer" onClick={handlerClose}>
+          <span className="icon-x"></span>
+        </div>
+        <div className="relative z-10">
+          <p className="text-white text-xs">Deposit liquidty to earn your Share of</p>
+          <h4 className="text-gradient text-2xl">Blast Gold, Blast Points and Fenix Orbits</h4>
+          <p className="text-white text-xs">Earn your share in
+            <span className="bg-outrageous-orange-400 px-2 mx-2">
+              ONE OF THE BIGGEST AIRDROPS
+            </span>
+            in crypto history
+          </p>
+        </div>
+        <Button variant="primary" className="relative z-10">
+          <span>Deposit Now</span>
+        </Button>
+    </div>
+  )
+}
+
+export default BlastBanner
--- src/components/Trade/LimitRange/index.tsx
+++ src/components/Trade/LimitRange/index.tsx
@@ -4,10 +4,12 @@ import Panel from '@/src/components/Trade/LimitRange/Panel'
 import TradeProcess from '@/src/components/Trade/Common/TradeProcess'
 import { LIMIT_PROCESS } from '../data'
 import Chart from '@/src/components/Chart'
+import BlastBanner from '@/src/components/Common/BlastBanner'
 
 const LimitRange = () => {
   return (
-    <div className="flex flex-col items-start gap-6 mb-4 xl:gap-10 xl:flex-row">
+    <div className="flex flex-col items-start gap-6 mb-4">
+      <BlastBanner />
       <div className="flex flex-col w-full">
         <div className="flex flex-wrap w-full gap-5 mb-10 xl:flex-nowrap">
           <Panel />
