3b2f68fadb30f6740448bd3d2bd6736aa5183277
josesantana.web <josesantana.web@gmail.com>
2024-04-16 17:55:08 -0400

    feat: medium posts

--- src/components/Landing/Insights/index.tsx
+++ src/components/Landing/Insights/index.tsx
@@ -3,15 +3,24 @@
 import React, { useEffect, useState } from 'react'
 import CardInsights from "./cardInsights"
 import Image from "next/image"
+
 const Insights = () => {
   const [posts, setPosts] = useState([])
-  const url = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40Fenix_Finance'
+  const url = 'https://api.medium.com/v1/users/18aa89c59c85aa5c279fd7222e4313ec2543fc7551b9301c18454dce3080ea279/publications'
+  const token = '298ead3a697d6288201e91e783026528cc494686d182bf295b4867e59e7dde73f'
 
   useEffect(() => {
-    fetch(url)
+    fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
     .then(response => response.json())
-    .then(data => setPosts(data.items))
-    .catch(error => console.error('Error:', error))
+    .then(data => {
+      console.log('posts', data)
+      setPosts(data.items)
+    })
+    .catch(error => console.error('Error publication:', error))
   }, [])
 
   return (
