042430f08d3a98deeda0a5694275698e45e8695f
0xSather <0xsather@proton.me>
2024-04-19 20:30:33 +0200

    Hide menu until launch

--- src/components/Common/Layout/Header/index.tsx
+++ src/components/Common/Layout/Header/index.tsx
@@ -15,7 +15,9 @@ const Header = () => {
   // Todas las clases que tienen como condicion "pathname === '/' son tomadas en cuenta para el landing page de forma que no modifiquen estilos importantes en el resto de la aplicación"
   return (
     <header className="hidden mx-auto xl:block container">
-      <div className={`${pathname === '/' ? '' : 'header-box px-5 pb-3'} flex justify-between items-center gap-5 rounded-l`}>
+      <div
+        className={`${pathname === '/' ? '' : 'header-box px-5 pb-3'} flex justify-between items-center gap-5 rounded-l`}
+      >
         <div className="flex items-center justify-between  relative z-10 h-[95px] 2xl:w-1/3 ">
           <Link href="/">
             {width < 940 ? (
@@ -39,9 +41,7 @@ const Header = () => {
             )}
           </Link>
         </div>
-        <div className={`${pathname === '/' ? 'w-1/3' : 'w-full'} relative z-10`}>
-          <Menu />
-        </div>
+        <div className={`${pathname === '/' ? 'w-1/3' : 'w-full'} relative z-10`}>{/* <Menu /> */}</div>
         <div
           className={`flex relative z-10 items-center gap-3.5 justify-end px-5 h-[95px]
           ${pathname === '/' ? '2xl:w-1/3 ' : '2xl:w-3/5'}`}
