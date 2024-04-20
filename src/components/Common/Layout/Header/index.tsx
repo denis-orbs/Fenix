46a759ca19628fc5e28f25ffde6b2b65301695dc
starketh25 <starketh25@gmail.com>
2024-04-20 20:56:06 +0530

    Header fix again

--- src/components/Common/Layout/Header/index.tsx
+++ src/components/Common/Layout/Header/index.tsx
@@ -14,9 +14,11 @@ const Header = () => {
 
   // Todas las clases que tienen como condicion "pathname === '/' son tomadas en cuenta para el landing page de forma que no modifiquen estilos importantes en el resto de la aplicación"
   return (
-    <header className="hidden mx-auto xl:block container">
-      <div className={`${pathname === '/' ? '' : 'header-box px-5 pb-3'} flex justify-between items-center gap-5 rounded-l`}>
-        <div className="flex items-center justify-between  relative z-10 h-[95px] 2xl:w-1/3 ">
+    <header className="hidden mx-auto md:block container">
+      <div
+        className={`${pathname === '/' ? '' : 'header-box px-5 pb-3'} flex justify-between items-center ${width < 860 ? 'gap-1' : 'gap-5'} rounded-l`}
+      >
+        <div className="flex items-center justify-between min-w-[50px] w-full z-10 h-[95px] 2xl:w-1/3 ">
           <Link href="/">
             {width < 940 ? (
               <Image
@@ -43,7 +45,7 @@ const Header = () => {
           <Menu />
         </div>
         <div
-          className={`flex relative z-10 items-center gap-3.5 justify-end px-5 h-[95px]
+          className={`flex relative z-10 items-center gap-3.5 justify-end ${width < 860 ? 'px-1' : 'px-5'} h-[95px]
           ${pathname === '/' ? '2xl:w-1/3 ' : '2xl:w-3/5'}`}
         >
           <AccountHandler isMenuMobile={false} />
