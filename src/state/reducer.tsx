e681febc492f16184332bb0f1d2d4dfe23c32e42
josnielfermin <josniel@esthetiqo.com>
2024-04-24 11:30:04 -0400

    fix

--- src/state/reducer.tsx
+++ src/state/reducer.tsx
@@ -4,14 +4,12 @@ import liquidity from './liquidity/reducer'
 import lock from './lock/reducer'
 import vote from './vote/reducer'
 import user from './user/reducer'
-// import chart from './swap-chart/reducer'
 
 const reducer = combineReducers({
   user,
   liquidity,
   lock,
   vote,
-  // chart,
 })
 
 export default reducer
