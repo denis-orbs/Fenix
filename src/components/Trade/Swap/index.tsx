9f1bc4bf9b2a4688305a6e1bbebb33f9f7b1388e
Herodes Lugo <herodeslugo@gmail.com>
2024-04-25 14:12:26 -0400

    feat: add notification remaining status

--- src/components/Trade/Swap/index.tsx
+++ src/components/Trade/Swap/index.tsx
@@ -11,10 +11,12 @@ const Swap = () => {
   return (
     <div className="flex flex-col items-start gap-6 mb-4 justify-center">
       <BlastBanner />
-      <div className="flex flex-col w-full">
+      <div className="relative z-50">
+      </div>
+      <div className="flex flex-col w-full ">
         <div className="flex flex-wrap w-full xl:gap-5 mb-10 xl:flex-nowrap justify-center mt-10">
           <Panel />
-          {showChart && <Chart token0='' token1='' />}
+          {showChart && <Chart token0="" token1="" />}
         </div>
       </div>
     </div>
