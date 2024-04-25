b571e87a2e436ad9b1cba37d9761c4722fdd3e1b
0xSather <0xsather@proton.me>
2024-04-25 17:15:24 +0200

    Remove unused imports

--- src/components/Trade/Swap/index.tsx
+++ src/components/Trade/Swap/index.tsx
@@ -1,8 +1,6 @@
 'use client'
 import { useShowChart } from '@/src/state/user/hooks'
 import Panel from '@/src/components/Trade/Swap/Panel'
-import TradeProcess from '@/src/components/Trade/Common/TradeProcess'
-import { TRADE_PROCESS } from '../data'
 import Chart from '@/src/components/Liquidity/Deposit/Chart'
 import BlastBanner from '@/src/components/Common/BlastBanner'
 
@@ -14,7 +12,7 @@ const Swap = () => {
       <div className="flex flex-col w-full">
         <div className="flex flex-wrap w-full xl:gap-5 mb-10 xl:flex-nowrap justify-center mt-10">
           <Panel />
-          {showChart && <Chart token0='' token1='' />}
+          {showChart && <Chart token0="" token1="" />}
         </div>
       </div>
     </div>
