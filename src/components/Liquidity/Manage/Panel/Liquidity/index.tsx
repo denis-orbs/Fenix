1ed3c9000dbaabac36a4a2c5426d11aaf26ded69
TriHaz <trihaz@proton.me>
2024-05-09 08:48:10 +0400

    100% withdraw check

--- src/components/Liquidity/Manage/Panel/Liquidity/index.tsx
+++ src/components/Liquidity/Manage/Panel/Liquidity/index.tsx
@@ -192,7 +192,9 @@ const Manage = ({}: {}) => {
     )
     setFirstValue(
       formatNumber(
-        positionData.amount0 == 0
+        withdrawPercent == 100 ?
+          Number(BigInt(positionData.amount0)) / 10 ** firstToken.decimals
+        : positionData.amount0 == 0
           ? 0
           : Number(
               (BigInt(positionData.amount0) * BigInt(10 ** 10)) /
@@ -203,7 +205,9 @@ const Manage = ({}: {}) => {
     )
     setSecondValue(
       formatNumber(
-        positionData.amount1 == 0
+        withdrawPercent == 100 ?
+          Number(BigInt(positionData.amount1)) / 10 ** secondToken.decimals
+        : positionData.amount1 == 0
           ? 0
           : Number(
               (BigInt(positionData.amount1) * BigInt(10 ** 10)) /
