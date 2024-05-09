f60fc8455179aea031299e61458308dc9be59651
0xNobu <168653134+0x-Nobu@users.noreply.github.com>
2024-05-09 19:12:50 +0400

    Update page.tsx

--- src/app/points-program/page.tsx
+++ src/app/points-program/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 
 export const metadata = {
-  title: 'Fenix Rings | Fenix Finance',
+  title: 'Rings | Fenix Finance',
   description:
     'Fenix Rings will be convertible into the Fenix Airdrop at our token generation event. Take a stake in the main trading and liquidity marketplace for Blast.',
 }
