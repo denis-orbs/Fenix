28cb31e4437c33fb40d9e92972094b53427b12e7
0xSather <0xsather@proton.me>
2024-05-24 21:09:11 +0200

    feat: Add dynamic export in ranking route

--- src/app/api/rings/ranking/route.ts
+++ src/app/api/rings/ranking/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { toBN } from '@/src/library/utils/numbers'
 import { NextRequest, NextResponse } from 'next/server'
 import prisma from '@/src/library/utils/db'
