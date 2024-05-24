119768cd319a480a64ee1b43980e2b97f0ea21c6
josnielfermin <josniel@esthetiqo.com>
2024-05-24 09:54:55 -0400

    feat: User row highlight on ranking table

--- src/components/PointsProgram/Leaderboard/Item.tsx
+++ src/components/PointsProgram/Leaderboard/Item.tsx
@@ -2,9 +2,9 @@
 import { RankingEntry } from '@/src/library/hooks/rings/useRingsPoints'
 import { formatAmount, formatCurrency } from '@/src/library/utils/numbers'
 import Image from 'next/image'
-const Item = ({ data }: { data: RankingEntry }) => {
+const Item = ({ data, isUser }: { data: RankingEntry, isUser: boolean }) => {
   return (
-    <div className="flex items-center w-full bg-shark-400 bg-opacity-40 border border-shark-300 py-6 rounded-md mb-4 px-3 xl:px-0 gap-3">
+    <div className={`flex items-center w-full ${isUser ? 'bg-shark-100' : 'bg-shark-400'} bg-opacity-40 border border-shark-300 py-6 rounded-md mb-4 px-3 xl:px-0 gap-3`}>
       <span className="text-white xl:w-36 text-center flex items-center justify-center">
         <div className="bg-shark-400 bg-opacity-40 border border-shark-300 w-10 rounded-md text-xs py-2">
           {data?.ranking}
