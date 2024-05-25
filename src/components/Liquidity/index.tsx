5c201286f4587912a6164a2de10c32265e3ed24b
0xSather <0xsather@proton.me>
2024-05-25 20:28:49 +0200

    feat: Update liquidity pool tabs
    This commit updates the filter options for the liquidity pool in the Liquidity component. It adds new options for different pool categories such as Liquidity Manager, Memes, GameFi, Staked & Restaked ETH, Blue Chips, Stablecoins, and Blast Ecosystem. The corresponding pool IDs are also updated accordingly.
    
    The commit message follows the conventional format of "feat: <description>".

--- src/components/Liquidity/index.tsx
+++ src/components/Liquidity/index.tsx
@@ -8,7 +8,17 @@ import { useAllPools } from '@/src/state/liquidity/hooks'
 import { BasicPool } from '@/src/state/liquidity/types'
 import { useEffect, useState } from 'react'
 import HeaderRow from './Tables/LiquidityTable/HeaderRow'
-import { OPTIONS_FILTER, STEPS } from './data'
+import {
+  BLAST_ECOSYSTEM_POOLS,
+  BLUE_CHIPS_POOLS,
+  GAMEFI_POOLS,
+  LIQUIDITY_MANAGER_POOLS,
+  MEMES_POOLS,
+  OPTIONS_FILTER,
+  STABLECOINS_POOLS,
+  STAKED_RESTAKED_ETH_POOLS,
+  STEPS,
+} from './data'
 
 const Liquidity = () => {
   const [currentTab, setCurrentTab] = useState<string>('ALL POOLS')
@@ -19,13 +29,29 @@ const Liquidity = () => {
 
   useEffect(() => {
     if (pools && pools?.length > 0) {
-      if (currentTab === 'VOLATILE') {
-        // fix filter. user the type of position pool in BasicPool interface
-        setFilteredPools([])
-      } else if (currentTab === 'STABLE') {
-        setFilteredPools([])
-      } else if (currentTab === 'CONCENTRATED') {
-        setFilteredPools(pools)
+      if (currentTab === 'LIQUIDITY MANAGER') {
+        const combinedPools = pools.filter((pool: BasicPool) => LIQUIDITY_MANAGER_POOLS.includes(pool.id.toLowerCase()))
+        setFilteredPools(combinedPools)
+      } else if (currentTab === 'MEMES') {
+        const combinedPools = pools.filter((pool: BasicPool) => MEMES_POOLS.includes(pool.id.toLowerCase()))
+        setFilteredPools(combinedPools)
+      } else if (currentTab === 'GAMEFI') {
+        const combinedPools = pools.filter((pool: BasicPool) => GAMEFI_POOLS.includes(pool.id.toLowerCase()))
+        setFilteredPools(combinedPools)
+      } else if (currentTab === 'STAKED & RESTAKED ETH') {
+        const combinedPools = pools.filter((pool: BasicPool) =>
+          STAKED_RESTAKED_ETH_POOLS.includes(pool.id.toLowerCase())
+        )
+        setFilteredPools(combinedPools)
+      } else if (currentTab === 'BLUE CHIPS') {
+        const combinedPools = pools.filter((pool: BasicPool) => BLUE_CHIPS_POOLS.includes(pool.id.toLowerCase()))
+        setFilteredPools(combinedPools)
+      } else if (currentTab === 'STABLECOINS') {
+        const combinedPools = pools.filter((pool: BasicPool) => STABLECOINS_POOLS.includes(pool.id.toLowerCase()))
+        setFilteredPools(combinedPools)
+      } else if (currentTab === 'BLAST ECOSYSTEM') {
+        const combinedPools = pools.filter((pool: BasicPool) => BLAST_ECOSYSTEM_POOLS.includes(pool.id.toLowerCase()))
+        setFilteredPools(combinedPools)
       } else {
         setFilteredPools(pools)
       }
@@ -54,7 +80,7 @@ const Liquidity = () => {
 
       <div className="flex flex-col justify-between gap-5 mb-4 md:items-center xl:flex-row overflow-hidden">
         <h5 className="text-lg mb-1 text-white max-xl:w-full lg:flex-shrink-0">Liquidity Pools</h5>
-        <div className='w-full'>
+        <div className="w-full">
           <Filter options={OPTIONS_FILTER} currentTab={currentTab} setCurrentTab={setCurrentTab} />
         </div>
         <div className="w-full xl:w-1/3">
