79d1ae8140967f54d62265ec48b37ac79836a53b
0xSather <0xsather@proton.me>
2024-05-29 01:23:44 +0200

    fix: fdao apr

--- src/library/hooks/web3/useFDAOEmisionsAPR.tsx
+++ src/library/hooks/web3/useFDAOEmisionsAPR.tsx
@@ -9,13 +9,19 @@ export default function useFDAOEmissionsAPR(row: BasicPool) {
   useEffect(() => {
     const fetchFDAOPrice = async () => {
       // fdao pool
-      if (row.id !== '0x886369748d1d66747b8f51ab38de00dea13f0101') return
+      if (row.id !== '0x886369748d1d66747b8f51ab38de00dea13f0101') {
+        setFDAOEmisionsAPR(0)
+        return
+      }
       const tokens = await fetchTokens()
       const fDAOTokenPrice = tokens.find(
         // fdao token address
         (token) => token.tokenAddress.toLowerCase() === '0x3b0cffda9a5ab64135c227638e777ceec0c243a8'
       )?.priceUSD
-      if (!fDAOTokenPrice) return
+      if (!fDAOTokenPrice) {
+        setFDAOEmisionsAPR(0)
+        return
+      }
       const apr = toBN(50_000)
         .multipliedBy(fDAOTokenPrice)
         .dividedBy(row.totalValueLockedUSD)
