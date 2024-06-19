0aadaeb9c85504b58692ab578bd7025f65b6d74e
josnielfermin <josniel@esthetiqo.com>
2024-06-19 14:07:44 -0400

    advances

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
+
 // Rutas que quieres desactivar
 const disabledRoutes = [
   '/trade/recurring',
@@ -7,14 +8,29 @@ const disabledRoutes = [
   '/lock',
   '/lock/(.*)',
   '/vote',
-  '/rewards'
-]
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
-  if (disabledRoutes.some(route => new RegExp(`^${route}$`).test(pathname))) {
-    return NextResponse.redirect(new URL('/not-found', request.url))  }
+  '/rewards',
+];
 
-  return NextResponse.next()}
+const isMobileDevice = (userAgent: string): boolean => {
+  const mobileRegex = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i;
+  return mobileRegex.test(userAgent);
+};
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const userAgent = request.headers.get('user-agent') || '';
+  const isMobile = isMobileDevice(userAgent);
+
+  if (disabledRoutes.some(route => new RegExp(`^${route}$`).test(pathname))) {
+    return NextResponse.redirect(new URL('/not-found', request.url));
+  }
+
+  if (isMobile && (pathname !== '/trade/swap' && pathname !== '/')) {
+    return NextResponse.redirect(new URL('/trade/swap', request.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ['/trade/recurring', '/trade/limit-range', '/trade/dca', '/lock', '/lock/(.*)', '/vote', '/rewards'],
