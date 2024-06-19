08c6d27fc44e48a34211b6455b63d129625851a3
0xSather <0xsather@proton.me>
2024-06-19 12:47:09 +0200

    Revert "fix: rezize point program table"
    This reverts commit 10546cffb3f7d6636b10d7d8dabe4ddc0a1424fa.

--- src/components/PointsProgram/Leaderboard/index.tsx
+++ src/components/PointsProgram/Leaderboard/index.tsx
@@ -52,11 +52,11 @@ const Leaderboard = () => {
       </div>
       <div className="relative">
         <div className="flex items-center w-full mb-3">
-          <span className="text-white w-[15%] text-center text-sm">Rank</span>
-          <span className="text-white w-[25%] text-sm">Ranking Addresses</span>
-          <span className="text-white w-[20%] text-sm text-center">Multiplier</span>
+          <span className="text-white w-[10%] text-center text-sm">Rank</span>
+          <span className="text-white w-[15%] text-sm">Ranking Addresses</span>
+          <span className="text-white w-[15%] text-sm text-center">Multiplier</span>
           <span className="text-white w-[20%] text-sm text-center">Gold Potential Rewards</span>
-          {/* <span className="text-white w-[20%] text-sm text-center">Fenix Gold Rings</span> */}
+          <span className="text-white w-[20%] text-sm text-center">Fenix Gold Rings</span>
           <span
             className="text-white w-[20%] cursor-pointer text-center text-sm flex items-center justify-center gap-x-2"
             onClick={() => handleSort()}
