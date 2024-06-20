f25ef8a5e49102bbfa063a80c4f0f970edd2a0ee
0xSather <0xsather@proton.me>
2024-06-20 17:07:24 +0200

    feat: add api nft bonus rings earned by an user

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -90,28 +90,38 @@ export async function GET(request: NextRequest) {
   const ichiProvider = new IchiProvider()
   const manualProvider = new ManualLiquidityProvider()
 
-  const [ichi, manualV3] = await Promise.all([
-    ichiProvider.getUserLiquidity(account),
-    manualProvider.getUserLiquidity(account),
-  ])
-  let totalTVL = BN_ZERO
-  let boostedTVL = BN_ZERO
+  // const [ichi, manualV3] = await Promise.all([
+  //   ichiProvider.getUserLiquidity(account),
+  //   manualProvider.getUserLiquidity(account),
+  // ])
+  // let totalTVL = BN_ZERO
+  // let boostedTVL = BN_ZERO
 
-  totalTVL = totalTVL.plus(ichi.TVL).plus(manualV3.TVL)
-  boostedTVL = boostedTVL.plus(ichi.boostedTVL).plus(manualV3.boostedTVL)
-
+  // totalTVL = totalTVL.plus(ichi.TVL).plus(manualV3.TVL)
+  // boostedTVL = boostedTVL.plus(ichi.boostedTVL).plus(manualV3.boostedTVL)
+  const nftBonus = await prisma.ring_bonus.aggregate({
+    where: {
+      user_id: account,
+    },
+    _sum: {
+      ring_points: true,
+      gold_points: true,
+    },
+  })
+  const userData = { ...user, nft_extra_rings_points: nftBonus._sum.ring_points }
   return NextResponse.json(
     {
       success: true,
-      user,
-      totalTVL: totalTVL.decimalPlaces(2).toString(),
-      totalBoostedTVL: boostedTVL.decimalPlaces(2).toString(),
-      liquidity: {
-        manualV3,
-        clm: {
-          ichi,
-        },
-      },
+      user: userData,
+      // totalTVL: totalTVL.decimalPlaces(2).toString(),
+      // totalBoostedTVL: boostedTVL.decimalPlaces(2).toString(),
+      // nftBonus,
+      // liquidity: {
+      //   manualV3,
+      //   clm: {
+      //     ichi,
+      //   },
+      // },
     },
     { status: 200 }
   )
