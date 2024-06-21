5004f1acfa05eaa6271d42237580d7441432aa47
0xSather <0xsather@proton.me>
2024-06-21 11:23:12 +0200

    Revert "Merge pull request #400 from Esthetiqo/hotfix/ranking-badge"
    This reverts commit 95ce1b6a4b3b74ee49376aa59b97f07e2c8f9456, reversing
    changes made to 7985c81562fc68c97a8d4b76c2600f8f04a781bb.

--- src/components/PointsProgram/Leaderboard/Item.tsx
+++ src/components/PointsProgram/Leaderboard/Item.tsx
@@ -5,43 +5,28 @@ import Image from 'next/image'
 const Item = ({ data, isUser }: { data: RankingEntry; isUser: boolean }) => {
   return (
     <div
-      className={`flex items-center w-full ${isUser ? 'bg-ranking-gradient border border-outrageous-orange-400' : 'bg-shark-400 border border-shark-300'} bg-opacity-40 py-6 rounded-md mb-4 xl:px-0`}
+      className={`flex items-center w-full ${isUser ? 'bg-ranking-gradient border border-outrageous-orange-400' : 'bg-shark-400 border border-shark-300'} bg-opacity-40 py-6 rounded-md mb-4 px-3 xl:px-0 gap-3`}
     >
-      <span className="text-white w-[15%] text-center flex items-center justify-center">
+      <span className="text-white xl:w-36 text-center flex items-center justify-center">
         <div
           className={`bg-shark-400 bg-opacity-40 border ${isUser ? 'border-white' : 'border-shark-300'} w-10 rounded-md text-xs py-2`}
         >
           {data?.ranking}
         </div>
       </span>
-      <span className="text-white w-[25%]">
-        <div className="flex items-center justify-center gap-4">
-          <p className="text-xs max-w-[100px] lg:max-w-auto truncate">{data.id}</p>
-        </div>
-      </span>
-      <span className="text-white w-[20%] flex justify-center">
-        <div className="bg-shark-400 text-xs border border-shark-100 rounded-xl px-6 py-1">{data?.nft_boost || 0}%</div>
-      </span>
-      <span className="text-white w-[20%] flex justify-center">
-        <div className="bg-shark-400 text-xs border w-[50%] border-shark-100 rounded-xl px-3 py-2 flex items-center justify-center gap-2">
+      <span className="text-white w-full">
+        <div className="flex items-center gap-4">
           <Image
-            src="/static/images/point-stack/blast-gold.svg"
+            src="/static/images/points-program/orbit.svg"
             alt="user"
             width={40}
             height={40}
-            className="w-4 h-4"
+            className="w-10 h-10"
           />
-          {formatCurrency(data?.gold_potential_rewards) || 0}
+          <p className="text-sm max-w-[100px] xl:max-w-auto truncate">{data.id}</p>
         </div>
       </span>
-      {/* <span className="text-white w-[20%] flex justify-center">
-        <div className="flex items-center gap-2 text-xs">
-          <Image src="/static/images/points-program/orbit.svg" alt="user" width={40} height={40} className="w-4 h-4" />
-          {formatAmount(data?.gold_qualifying_rings, 6, true) || 0}
-        </div>
-      </span> */}
-      <span className="text-white flex items-center justify-center gap-3 xl:w-[20%] text-center text-xs">
-        <Image src="/static/images/points-program/orbit.svg" alt="user" width={40} height={40} className="w-4 h-4" />
+      <span className="text-white xl:w-36 text-center text-sm">
         {formatAmount(data.accumulated_rings_points, 6, true)}
       </span>
     </div>
--- src/library/hooks/web3/useIchi.ts
+++ src/library/hooks/web3/useIchi.ts
@@ -128,7 +128,6 @@ export const useIchiPositions = () => {
             const chain = SupportedChainId.blast
 
             const vaultInfo = await getIchiVaultInfo(chain, dex, item.vaultAddress)
-            console.log('vaultInfo >> ', vaultInfo)
             const tokenAid = vaultInfo.tokenA
             const tokenBid = vaultInfo.tokenB
             //const vaultInfo = useIchiVaultsData(item.vaultAddress)
